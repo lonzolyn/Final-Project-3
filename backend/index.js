653c77ef2f9812e88757fd4f1f484b4547d9df01
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-09-20 14:24:57 -0400

    set up config

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
 const app = express()
+
+const PORT = process.env.PORT || 8080
+
 
 app.get("/api", (req,res)=> {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
 
-app.listen(8000, () => { console.log("Server started on port 8000")})
+app.listen(PORT, console.log(`listening on port ${PORT}`))
 
 //added a line//
