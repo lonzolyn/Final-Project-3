f1c293a7fedbf121143b0d4e508c4e0879fa121f
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-09-23 12:09:58 -0400

    set up config and basic express

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,27 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
 
+
+
 const app = express()
 
+//middlewares
+app.use(express.json())
+
+//routes
+
+
+//database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })  
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
+
+//app.get("/api", (req,res)=> {
+   // res.json({"users": ["userOne", "userTwo", "userThree"]})
+//})
+
+
 const PORT = process.env.PORT || 8080
-
-
-app.get("/api", (req,res)=> {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
 
