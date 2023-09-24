3730c07872fd2e41dfaab2b46ef9e6c5a0fe0be2
lonzolyn <99560335+lonzolyn@users.noreply.github.com>
2023-09-24 14:05:45 -0400

    models

--- backend/models/park.js
+++ backend/models/park.js
@@ -11,8 +11,8 @@ const parkSchema = new mongoose.Schema({
         type: String,  
         default: 'https://thumbs.dreamstime.com/b/bread-cut-14027607.jpg' 
     },
-     baker: { 
-      type: mongoose.Types.ObjectId,
-     ref: 'Park' } 
+    // baker: { 
+     // type: mongoose.Types.ObjectId,
+    //ref: 'Park' } 
 })
 module.exports = mongoose.model('Park', parkSchema)
